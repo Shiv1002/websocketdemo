9940f55c2fc20cdc4d6bdcc0cf82a60df6151337
shivank <shivank.pasi@somaiya.edu>
2023-12-22 11:46:47 +0530

    update app.jsx to talk to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,35 +4,53 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [socket, setSocket] = useState(null);
-
+  const [isOpen, setOpen] = useState(false);
+  const [msg, setMsg] = useState([]);
   useEffect(() => {
     const url = "ws://localhost:1200";
     const ws = new WebSocket(url);
     setSocket(ws);
     ws.onopen = () => {
       console.log("server is Listening!");
+      // setting up state of input dom according to server
+      setOpen(true);
     };
     ws.onmessage = (e) => {
-      console.log(JSON.parse(e.data));
+      //getting data in json string
+      setMsg(JSON.parse(e.data));
     };
     ws.onerror = () => {
       console.log("error occured!");
+      setOpen(false);
     };
   }, []);
 
   function sendMessage() {
-    if (text) socket.send(text);
+    if (text) {
+      socket.send(text);
+      setText("");
+    }
   }
+
   return (
     <>
       <h2>This is Websocket demo app</h2>
+      <div className="msg-container">
+        {msg.map((ele, index) => {
+          // ele is string
+          return <li key={index}>{ele}</li>;
+        })}
+      </div>
+
       <input
         type="text"
         onChange={(e) => setText(e.target.value)}
         value={text}
         min={2}
       />
-      <button onClick={() => sendMessage()}>Send</button>
+      <button onClick={() => sendMessage()} disabled={!isOpen}>
+        Send
+      </button>
     </>
   );
 }
